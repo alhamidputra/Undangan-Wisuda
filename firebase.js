98306d2d79413afd9b13f42f989bf1466f6eaab5
alhamidputra <alhamidputra54@gmail.com>
2026-07-11 01:23:54 +0700

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,21 @@
+// Import Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+
+import {
+    getFirestore
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCL7PjhQFfU_527-0qRQSZabffoxUvRL-M",
+  authDomain: "undangan-wisuda-al.firebaseapp.com",
+  projectId: "undangan-wisuda-al",
+  storageBucket: "undangan-wisuda-al.firebasestorage.app",
+  messagingSenderId: "836790173166",
+  appId: "1:836790173166:web:2682f142916db74041a2ff"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export { db };
